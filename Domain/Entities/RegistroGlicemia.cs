42e260fc70b0fd13f8b38db9f5aaa6ba8c4dadb6
dieisson <dieisson.qfloriano@gmail.com>
2026-08-31 09:33:58 -0300

    Estrutura inicial do backend

---
+++ Domain/Entities/RegistroGlicemia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RegistroGlicemia
+    {
+        public int Id { get; set; }
+        public int Glicemia {  get; set; }
+        public decimal Dose { get; set; }
+        public TimeSpan Hora { get; set; }
+        public string Refeicao { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string TipoDiabetes { get; set; } = string.Empty;
+        public int? Idade { get; set; }
+        public int? Celular { get; set; }
+        public List<RegistroGlicemia> RegistroGlicemia { get; set; } = new ();
+    }
+}
