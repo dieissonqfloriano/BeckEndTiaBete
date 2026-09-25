50dccf7dff81ba6e1b1a311d11f9e59a5f6c70d5
dieisson <dieisson.qfloriano@gmail.com>
2026-09-25 19:34:32 -0300

    Adicionado campos obrigatorios e limites no UsuarioCreateDto

--- Application/DTOs/UsuarioCreateDto.cs
+++ Application/DTOs/UsuarioCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,31 @@ namespace Application.DTOs
 {
     public class UsuarioCreateDto
     {
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
         public string Senha { get; set; } = string.Empty;
+
+        [Required]
         public string TipoDiabetes { get; set; } = string.Empty;
+
+        [Range(1, 120)]
         public int? Idade { get; set; }
+
+
         public string? Celular { get; set; }
+
+        [Range(1, 600)]
         public int FatorSensibilidade { get; set; }
+
+        [Range(1, 600)]
         public int HgtAlvo { get; set; }
     }
 }
